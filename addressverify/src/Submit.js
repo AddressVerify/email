b4eb054ff1e4dbc21f6afc51ae9b12b14c744180
Cameron Sobhani <xvxcxvx@gmail.com>
2020-04-06 16:15:39 -0500

    pushing to master

--- addressverify/src/Submit.js
+++ addressverify/src/Submit.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import CSV from 'react-csv-reader';
+import { CSVLink, CSVDownload } from "react-csv";
+import Axios from "axios";
+
 
 class Submit extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			submission: ''
+			submission: 'fire,water,air,dirt',
+			task: ['lmao','fire','water'],
+
 		}
 
 	}
@@ -19,6 +24,7 @@ class Submit extends React.Component {
 	}
 
 	handleSubmit(e) {
+
 		//submit to DB
 		//read from DB
 		//run verification functions
@@ -28,6 +34,11 @@ class Submit extends React.Component {
 		// let trimStr = testStr.replace(/\s+/g, '')
 		let testArr = testStr.split(/[\n,]/);
 		console.log(testArr)
+	}
+	handleImport(e) {
+		this.setState({
+			task: e,
+		}, console.log(this.state.task, e))	
 	}
 
 	render() {
@@ -42,7 +53,9 @@ class Submit extends React.Component {
 					<button onClick={() => this.handleSubmit()}>Submit!</button>
 				</div>
 				<p>CSV Import</p>
-				<CSV onFileLoaded={(data, fileInfo) => console.dir(data, fileInfo)} />
+				<CSV onFileLoaded={(data, fileInfo) => this.handleImport(data)}/>
+				<button onClick={()=> console.log(this.state.task)} />
+				<CSVLink data={this.state.submission}>Download me</CSVLink>;
 			</div>
 		);
 	}
