e780d656d8f7b0cc5f0e6ab73f655b5217c5d132
Cameron Sobhani <xvxcxvx@gmail.com>
2020-04-06 18:31:02 -0500

    server basics with proxy and db call

--- addressverify/server/server.js
+++ addressverify/server/server.js
@@ -3,9 +3,14 @@ const path = require('path');
 const port = 4523;
 const app = express();
 
+const db = require("../database/db");
 app.use(express.static(path.join(__dirname + '../public')));
 app.use(express.json());
 
+
+app.post('/user', (req, res)=>{
+    db.userWrite()
+});
 
 app.listen(port, () => {
     console.log(`I can hear you at  ${port}`);
