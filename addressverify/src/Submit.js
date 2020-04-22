2733ee40ab8ca6b9bceb040b432077063fc94278
MilesSobhani <38140009+MilesSobhani@users.noreply.github.com>
2020-04-21 22:52:39 -0500

    changed UI and state to be clearer and utilize results

--- addressverify/src/Submit.js
+++ addressverify/src/Submit.js
@@ -9,7 +9,9 @@ class Submit extends React.Component {
 		super(props);
 		this.state = {
 			submission: 'fire,water,air,dirt',
-			task: ['lmao','fire','water'],
+			emails:[],
+			verified:[],
+			task: ['lmao','fire','water']
 
 		}
 
@@ -22,25 +24,36 @@ class Submit extends React.Component {
 		})
 	}
 
+	handleVerify(){
+		Axios.post('/verify', {
+			data:this.state.emails,
+			inactive:[1],
+			jobSet:null
+		})
+		.then((res) => {console.log(res)})
+		.catch((err) => {console.log(err)})
+		.finally((res) => this.setState({
+			emails:[],
+			verified:res
+		}))
+	}
+
 	handleSubmit(e) {
 		//submit to DB
 		let testStr = this.state.submission;
 		// let trimStr = testStr.replace(/\s+/g, '')
 		let testArr = testStr.split(/[\n,]/);
-
+		this.setState({
+			emails:testArr,
+			submission:''
+		})
 		// Axios.post('/user', {
 		// 	data:this.state
 		// })
 		// .then((res) => {console.log(res)})
 		// .then((verify(e)))
 		// .catch((err) => {console.log(err)})
-		Axios.post('/verify', {
-			data:testArr,
-			inactive:[1],
-			jobSet:null
-		})
-		.then((res) => {console.log(res)})
-		.catch((err) => {console.log(err)})
+
 		//read from DB
 		//run verification functions
 		//initiate progress bar
@@ -60,19 +73,27 @@ class Submit extends React.Component {
 	handleImport(e) {
 		this.setState({
 			task: e,
-		}, console.log(this.state.task, e))	
+		})
 	}
 
 	render() {
 		return (
 			<div>
 				<div>
+					<p>Total Submissions Entered</p>
+					{this.state.emails.length}
 					<p>Input Email Addresses</p>
 					<p>Single</p>
-					<textarea onChange={(e) => this.handleChange(e)}></textarea>
+					<textarea value={this.state.submission} onChange={(e) => this.handleChange(e)}></textarea>
+					<div>
+					</div>
 				</div>
 				<div>
-					<button onClick={() => this.handleSubmit()}>Submit and verify!</button>
+					<button onClick={() => this.handleSubmit()}>Submit</button>
+					<button onClick={() => this.handleVerify()}>Verify Submissions</button>
+					<p>
+					 {this.state.verified}
+					</p>				
 				</div>
 				<p>CSV Import</p>
 				<CSV onFileLoaded={(data, fileInfo) => this.handleImport(data)}/>
