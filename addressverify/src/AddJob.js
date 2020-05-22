097ea180999f8db1047b2ab382daaba700f26f3e
MilesSobhani <38140009+MilesSobhani@users.noreply.github.com>
2020-05-22 15:30:59 -0500

    ui updates

--- addressverify/src/AddJob.js
+++ addressverify/src/AddJob.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AddJob(props){
   return (
-    <div className={"job"}>
+    <div className={"collection"}>
       <button className={"button is-primary wide"}>Add New Job</button>
     </div>
   );
--- addressverify/src/Collections.js
+++ addressverify/src/Collections.js
@@ -5,13 +5,12 @@ import AddCollection from './AddCollection'
 export default function Collections(props){
   return (
     <div>
-      <AddCollection/>
       {props.collections.map((coll) => 
       <div className={"collection"}>
         <Collection data={coll}/>
       </div>
       )}
-
+      <AddCollection/>
     </div>
   );
 }
