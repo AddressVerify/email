89b4ee348e2efb9811a16bb26f85a48faf8ee9e8
MilesSobhani <38140009+MilesSobhani@users.noreply.github.com>
2020-05-25 11:47:25 -0500

    built drawer function for collections

--- addressverify/src/Collection.js
+++ addressverify/src/Collection.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Job from './Job';
 import AddJob from './AddJob';
 
-export default function Collection(props){
+export default function Collection(props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDrawer = () => setIsOpen(!isOpen);
   return (
     <>
-    <div className={'coll-display'}>
-      {props.data.name}
-    </div>
-
-    {props.data.jobs.map((job) =>
-    <div className={"job"}>
-      <Job data={job}/>
-    </div>
-    )}
-    <AddJob/>
+      <div className={'coll-display button is-primary wide level'} onClick={toggleDrawer}>
+        <div className={"level-left"}>
+          {props.data.name}
+        </div>
+        <div className={"level-right"}>
+        {props.data.collValid}/{props.data.collTotal}
+        </div>
+      </div>
+      {isOpen ?
+        props.data.jobs.map((job) =>
+          <div className={"job"}>
+            <Job data={job} />
+          </div>
+        )
+        : ''}
+      {isOpen ?
+        <AddJob />
+        : ''}
     </>
   );
 }
